734b71f65e312d38e715fdfccfd50f90234b88af
Juan Tovar <jujotomo@gmail.com>
2021-01-22 06:41:24 -0800

    Skeleton ready, pending scraper implementation

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
 
 from common import config
+
+#logger reference
+logger = logging.getLogger(__name__)
+
+def _news_scraper(news_site_uid):
+    #acces the config already loaded
+    host = config()['news_sites'][news_site_uid]['url']
+
+    logging.info(f'Beggining scraper for {host}')
 
 if __name__ == "__main__":
     #Define the args parser for the command line app
